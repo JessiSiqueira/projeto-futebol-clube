ad1eb04ad16dfb62179f30d0263776f3f12f49dc
Jessica <jessica.siqueira.rj@gmail.com>
2024-02-11 14:30:19 -0300

    será

--- app/backend/src/utils/Classification.ts
+++ app/backend/src/utils/Classification.ts
@@ -3,19 +3,19 @@ import { ILeaderBoard } from '../Interfaces/ILeaderBoard';
 const classificationOfTeam = (classification: ILeaderBoard[]): ILeaderBoard[] => {
   const sort = classification.sort((x, y) => {
     if (x.totalPoints !== y.totalPoints) {
-      return x.totalPoints - y.totalPoints;
+      return y.totalPoints - x.totalPoints;
     }
 
     if (x.totalVictories !== y.totalVictories) {
-      return x.totalVictories - y.totalVictories;
+      return y.totalVictories - x.totalVictories;
     }
 
     if (x.goalsBalance !== y.goalsBalance) {
-      return x.goalsBalance - y.goalsBalance;
+      return y.goalsBalance - x.goalsBalance;
     }
 
     if (x.goalsFavor !== y.goalsFavor) {
-      return x.goalsFavor - y.goalsFavor;
+      return y.goalsFavor - x.goalsFavor;
     }
 
     return 0;
